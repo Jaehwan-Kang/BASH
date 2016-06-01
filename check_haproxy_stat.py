845d0a1999a6096026cc94c99e86ba2ba6176c4e
Jaehwna-Kang <lupang1085@hanmail.net>
2016-06-01 15:51:13 +0900

    update) exception , list in

--- check_haproxy_stat.py
+++ check_haproxy_stat.py
@@ -5,28 +5,30 @@
 #
 # Example
 # > python check_haproxy_stat.py 211.61.155.206:1936 cyebiz Cyebizadmin151 lg-homeboy01
-
 import sys
 from haproxystats import HAProxyServer
 
+haproxy = HAProxyServer(sys.argv[1], sys.argv[2], sys.argv[3], timeout=3)
+
+listner = {}
+
+for b in haproxy.listeners:
+    listner[b.name] = b.status
+
 try:
-
-    haproxy = HAProxyServer(sys.argv[1], sys.argv[2], sys.argv[3], timeout=3)
-    for b in haproxy.listeners:
-        if b.name == sys.argv[4]:
-            if b.status == "UP":
-                print("%s : %s" % (b.name, b.status))
-                sys.exit(0)  # OK exit code
-            elif b.status == "DOWN":
-                print("%s : %s" % (b.name, b.status))
-                sys.exit(2)  # Critical exit code
-            else:
-                print("UNKNOWN %s : %s" % (b.name, b.status))
-                sys.exit(3)  # UNKNOWN exit code
+    if sys.argv[4] in listner:
+        if listner[sys.argv[4]] == "UP":
+            print("%s : %s" % (sys.argv[4], listner[sys.argv[4]]))
+            sys.exit(0)  # OK exit code
+        elif listner[sys.argv[4]] == "DOWN":
+            print("%s : %s" % (sys.argv[4], listner[sys.argv[4]]))
+            sys.exit(2)  # Critical exit code
         else:
-            print("No Match Listener")
-            sys.exit(3)
-
+            print("UNKNOWN %s : %s" % (sys.argv[4], listner[sys.argv[4]]))
+            sys.exit(3)  # Unknown exit code
+    else:
+        print("Not Matched Listner : %s" % sys.argv[4])
+        sys.exit(3) # Unknown exit code
 except IndexError as e:
-    print("Wrong ARG : %s" % e)
-    sys.exit(3)
+    print("Not Matched Arguments")
+    sys.exit(3) # Unknown exit code
